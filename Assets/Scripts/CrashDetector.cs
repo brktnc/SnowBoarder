01c38e89c3af86db6f65587c333180aa32976fe1
Berk <berk77_@hotmail.com>
2023-02-21 02:25:29 +0300

    LastChange

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.12f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    CircleCollider2D playerHead;
+   
+    public bool hasCrashed = false;
+    void Start()
+    {
+       playerHead = GetComponent<CircleCollider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Ground" && playerHead.IsTouching(other.collider) && !hasCrashed)
+        {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
